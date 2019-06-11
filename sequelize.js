8f8a82d73ef1ea67ef55b0491666acfc005c87eb
Matt <matthew.wong@outlook.com>
2019-06-11 15:04:38 -0400

    Secrets file is no longer necessary

--- sequelize.js
+++ sequelize.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 // const sequelize = new Sequelize()
 // console.log(process.env.databaseURI);
-let content = JSON.parse(fs.readFileSync("./secrets.json"));
+// let content = JSON.parse(fs.readFileSync("./secrets.json"));
 const sequelize = new Sequelize(process.env.databaseURI, {dialect:"postgres"});
 
 
