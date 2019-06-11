5827492b13447471d4ff88d221d12dffb99d1bb9
Matt <matthew.wong@outlook.com>
2019-06-11 15:36:35 -0400

    Attempt for CORS fix

--- app.js
+++ app.js
@@ -10,7 +10,24 @@ const bodyParser = require('body-parser');
 const routes = require('./src/routes');
 
 // Taken from StackOverflow https://stackoverflow.com/a/12008719
+app.use(function (req, res, next) {
 
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', '*');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
+   // Pass to next layer of middleware
+   next();
+});
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
